27bc822bcce744dd6e15e1717952b7f11c6d39f8
Agnes Chang <kradeki@gmail.com>
2013-03-18 11:33:24 -0400

    [emacs] added functionality clone of eclipse's move line/region up and down by line

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -84,3 +84,41 @@
 (tool-bar-mode -1)
 (setq fill-column 80)
 (setq lazy-highlight-cleanup nil)
+
+;; port of eclipse move text line/region
+; source: https://groups.google.com/d/msg/gnu.emacs.help/dd2R_UV0LVQ/F06ihLb7hKcJ
+(defun move-text-internal (arg)
+  (cond
+   ((and mark-active transient-mark-mode)
+    (if (> (point) (mark))
+	(exchange-point-and-mark))
+    (let ((column (current-column))
+	  (text (delete-and-extract-region (point) (mark))))
+      (forward-line arg)
+      (move-to-column column t)
+      (set-mark (point))
+      (insert text)
+      (exchange-point-and-mark)
+      (setq deactivate-mark nil)))
+   (t
+    (beginning-of-line)
+    (when (or (> arg 0) (not (bobp)))
+      (forward-line)
+      (when (or (< arg 0) (not (eobp)))
+	(transpose-lines arg))
+      (forward-line -1)))))
+
+(defun move-text-down (arg)
+     "Move region (transient-mark-mode active) or current line
+  arg lines down."
+     (interactive "*p")
+     (move-text-internal arg))
+
+(defun move-text-up (arg)
+     "Move region (transient-mark-mode active) or current line
+  arg lines up."
+     (interactive "*p")
+     (move-text-internal (- arg)))
+
+(global-set-key (kbd "C-c <up>") 'move-text-up)
+(global-set-key (kbd "C-c <down>") 'move-text-down)
