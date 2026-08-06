ceaafdb58952537fa46958bc9a597cbf48bd3848
Agnes Chang <agness@fastmail.com>
2026-08-06 22:41:43 +0900

    modernize editor settings + cleanup

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,135 +1,46 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-(setq package-list '(
-                     helm
-;                     ein ;ipython
-;                     auctex-latexmk
-;                     color-theme-solarized
-                     ))
-(package-initialize)
+;; Packages
+;; Install with M-x package-install-selected-packages.
+(setq package-selected-packages '(solarized-theme))
 
-; fetch packages
-(unless package-archive-contents
-  (package-refresh-contents))
+(when (package-installed-p 'solarized-theme)
+  (load-theme 'solarized-dark t))
 
-; install missing packages
-(dolist (p package-list)
-  (unless (package-installed-p p)
-    (package-install p)))
+;; Interface
 
-; use ido for files
-(ido-mode 1)
-(setq ido-everywhere t)
-(setq ido-enable-flex-matching t)
+(setq inhibit-startup-screen t
+      visible-bell t
+      column-number-mode t
+      use-short-answers t)
 
-; change all prompts to y or n
-(fset 'yes-or-no-p 'y-or-n-p)
-
-; centralize backup and autosave files in tmp dir
-(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
-
-; editor aesthetics
-(electric-pair-mode t)
-(show-paren-mode 1)
-(setq column-number-mode t) ; show col position
-(setq visible-bell t)
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
-(setq indent-line-function 'insert-tab)
-
-; delete trailing whitespace on save
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-; require final newline so github stops complaining once and for all
-(setq require-final-newline t)
-
-; auto wrap lines > 80 characters, else make big ugly green highlight
-(defun eighty-char-hook ()
-  (auto-fill-mode t)
-  (set-fill-column 80)
-  (highlight-lines-matching-regexp ".\\{81\\}" 'hi-green-b))
-(add-hook 'prog-mode-hook 'eighty-char-hook)
-
-; use solarized color scheme
-; (load-theme 'solarized t)
-; (setq frame-background-mode 'dark)
-
-; window aesthetics
-(setq inhibit-startup-screen t)
 (menu-bar-mode -1)
-(setq fill-column 80)
-(setq lazy-highlight-cleanup nil)
+(show-paren-mode 1)
+(electric-pair-mode 1)
 
-; ergoemacs
-(global-set-key (kbd "C-z") 'undo) ; ctrl-z
-(global-set-key (kbd "C-Z") 'redo) ; ctrl-shift-z
-(global-set-key (kbd "<f2>") 'xah-cut-line-or-region)
-(global-set-key (kbd "<f3>") 'xah-copy-line-or-region)
-(global-set-key (kbd "<f4>") 'yank)       ; paste
-(global-set-key (kbd "<C-f4>") 'yank-pop) ; paste previous
-(global-set-key (kbd "<f5>") 'undo)
-(global-set-key (kbd "<C-f5>") 'redo)
-;; cursor movement with right hand home-row
-(global-set-key (kbd "M-i") 'previous-line)
-(global-set-key (kbd "M-j") 'backward-char)
-(global-set-key (kbd "M-k") 'next-line)
-(global-set-key (kbd "M-l") 'forward-char)
-(global-set-key (kbd "M-u") 'backward-word)
-(global-set-key (kbd "M-o") 'forward-word)
-(global-set-key (kbd "M-SPC") 'set-mark-command)
-;; map M-x to menu/application key
-(define-key key-translation-map (kbd "C-p") (kbd "<menu>")) ; is C-p on mac
-;(global-set-key (kbd "<menu>") 'execute-extended-command) ; see helm
+;; Built-in completion, minibuffer history
+(fido-mode 1)
+(savehist-mode 1)
 
-; port of eclipse move text line/region
-; source: https://groups.google.com/d/msg/gnu.emacs.help/dd2R_UV0LVQ/F06ihLb7hKcJ
-(defun move-text-internal (arg)
-  (cond
-   ((and mark-active transient-mark-mode)
-    (if (> (point) (mark))
-        (exchange-point-and-mark))
-    (let ((column (current-column))
-        (text (delete-and-extract-region (point) (mark))))
-      (forward-line arg)
-      (move-to-column column t)
-      (set-mark (point))
-      (insert text)
-      (exchange-point-and-mark)
-      (setq deactivate-mark nil)))
-   (t
-    (beginning-of-line)
-    (when (or (> arg 0) (not (bobp)))
-      (forward-line)
-      (when (or (< arg 0) (not (eobp)))
-        (transpose-lines arg))
-      (forward-line -1)))))
-(defun move-text-down (arg)
-     "Move region (transient-mark-mode active) or current line arg lines down."
-     (interactive "*p")
-     (move-text-internal arg))
-(defun move-text-up (arg)
-     "Move region (transient-mark-mode active) or current line arg lines up."
-     (interactive "*p")
-     (move-text-internal (- arg)))
-(global-set-key (kbd "C-c <up>") 'move-text-up)
-(global-set-key (kbd "C-c <down>") 'move-text-down)
+;; Editing
 
-;; move to window spatially
-(global-set-key (kbd "C-c j") 'windmove-left)
-(global-set-key (kbd "C-c l") 'windmove-right)
-(global-set-key (kbd "C-c i") 'windmove-up)
-(global-set-key (kbd "C-c k") 'windmove-down)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (color-theme-solarized))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq-default indent-tabs-mode nil
+              tab-width 2
+              fill-column 80
+              require-final-newline t)
+
+;; Show the 80-column boundary
+(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
+
+;; Del trailing spaces on save except in prose and markup files.
+(defun clean-code-before-save ()
+  "Delete trailing whitespace when saving a programming buffer."
+  (when (derived-mode-p 'prog-mode)
+    (delete-trailing-whitespace)))
+(add-hook 'before-save-hook #'clean-code-before-save)
+
+;; Spatial window movement.
+(global-set-key (kbd "C-c j") #'windmove-left)
+(global-set-key (kbd "C-c l") #'windmove-right)
+(global-set-key (kbd "C-c i") #'windmove-up)
+(global-set-key (kbd "C-c k") #'windmove-down)
+
+(provide 'init)
--- zshrc
+++ zshrc
@@ -1,23 +1,19 @@
-eval "$(/opt/homebrew/bin/brew shellenv)" # for MBP M1
-
 PROMPT='%F{cyan}:%1~ ∮ %F{default}'
-# deprecated bash prompt
-#export PS1="\[\e[0;36m\]:\W ∮ \[\e[0m\]"
-#export PS2="\[\e[0;40m\]> \[\e[0m\]"
 
-export TERM=xterm # xterm-256color looks like eggplant mush if iterm2 solarized
 export EDITOR="emacs -nw"
 
-cd ~/Documents
+cd ~/Code
 alias e="emacs -nw"
 alias g="git"
 alias c="code" # visstudio
 alias o="open"
-alias ia="open $1 -a /Applications/iA\ Writer.app"
+ia() {
+    open "$1" -a "/Applications/iA Writer.app"
+}
 alias pyserve="python3 -m http.server"
 alias jsserve="http-server -p 8000"
 alias py="python3"
-alias pyramidshader="Java -jar /Applications/PyramidShader.jar"
+alias pyramidshader="java -jar /Applications/PyramidShader.jar"
 
 # birdkit shortcuts
 bb()
@@ -39,33 +35,6 @@ alias brpub="bb run pub"
 alias brpromo="bb run promos"
 alias brpage="bb run page"
 
-# python paths
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
+# python + ruby paths
+eval "$(pyenv init - zsh)"
 eval "$(rbenv init - zsh)"
-
-# for compilers to find zlib
-#export LDFLAGS="-L/usr/local/opt/zlib/lib"
-#export CPPFLAGS="-I/usr/local/opt/zlib/include"
-export LDFLAGS="-L/opt/homebrew/opt/zlib/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/zlib/include"
-export PKG_CONFIG_PATH="/opt/homebrew/opt/zlib/lib/pkgconfig"
-
-# load nvm + nvm bash comp
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-# Project-specific aliases
-alias pg_start="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
-alias cd_hello="cd /Users/agneschang/Documents/2023-01-01-hello-world"
-
-# ncurses
-export PATH="/opt/homebrew/opt/ncurses/bin:$PATH"
-
-
-# Artifactory config for @nyt, @nyt-cms & @newsdev npm registry. Configured: xxxx
-export ARTIFACTORY_USER=""
-export ARTIFACTORY_API_KEY=""
-export ARTIFACTORY_EMAIL=""
